8604f4186b2bb81339c4373141e70a54e8c4c66a
kkarki7120 <kkarki7120@gmail.com>
2024-07-29 17:53:26 +0545

    changes

--- server.js
+++ server.js
@@ -12,11 +12,11 @@ app.use(cors());
 
 // MySQL connection
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'reactuser', // Replace with your MySQL username
-  password: '12345', // Replace with your MySQL password
-  database: 'react',
-  port: 3306
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD, 
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
 db.connect((err) => {
