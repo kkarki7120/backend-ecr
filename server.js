f6051bd18baa9751fe0b6ae3726f6734da872d5b
kkarki7120 <kkarki7120@gmail.com>
2024-07-29 17:55:40 +0545

    changes

--- server.js
+++ server.js
@@ -74,4 +74,5 @@ app.post('/submit', (req, res) => {
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  console.log(`This is Dev`);
 });
